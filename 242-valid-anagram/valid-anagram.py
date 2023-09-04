81a8e662cee3a05477acd8cf3de3348c068a2968
Bharathkanapareddy12 <136273934+Bharathkanapareddy12@users.noreply.github.com>
2023-09-04 14:48:34 +0530

    Time: 38 ms (56.60%) | Memory: 14.4 MB (49.36%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False    
+                
+
